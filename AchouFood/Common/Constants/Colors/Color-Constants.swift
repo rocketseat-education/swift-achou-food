c18c21674cb35647687c3c58c7476aa1cefd1494
arthur <arthur.rios007@gmail.com>
2025-10-21 15:09:22 -0300

    Primeiras Views - Primeiras Views

--- AchouFood/Common/Constants/Colors/Color-Constants.swift
+++ AchouFood/Common/Constants/Colors/Color-Constants.swift
@@ -13,4 +13,11 @@ struct Color {
     static let redTransparent: UIColor = UIColor(red: 232 / 255, green: 41 / 255, blue: 51 / 255, alpha: 0.7)
     static let successBase: UIColor = UIColor(red: 6 / 255, green: 159 / 255, blue: 98 / 255, alpha: 1.0)
     static let successTransparent: UIColor = UIColor(red: 6 / 255, green: 159 / 255, blue: 98 / 255, alpha: 0.8)
+    
+    static let gray100: UIColor = UIColor(red: 250 / 255, green: 250 / 255, blue: 250 / 255, alpha: 1.0)
+    static let gray200: UIColor = UIColor(red: 235 / 255, green: 235 / 255, blue: 235 / 255, alpha: 1.0)
+    static let gray300: UIColor = UIColor(red: 224 / 255, green: 220 / 255, blue: 220 / 255, alpha: 1.0)
+    static let gray400: UIColor = UIColor(red: 92 / 255, green: 101 / 255, blue: 102 / 255, alpha: 1.0)
+    static let gray500: UIColor = UIColor(red: 66 / 255, green: 58 / 255, blue: 58 / 255, alpha: 1.0)
+    static let gray600: UIColor = UIColor(red: 31 / 255, green: 24 / 255, blue: 24 / 255, alpha: 1.0)
 }
--- AchouFood/Scenes/HomeDelivery/HomeDeliveryView.swift
+++ AchouFood/Scenes/HomeDelivery/HomeDeliveryView.swift
@@ -6,8 +6,27 @@
 //
 
 import UIKit
+import SnapKit
+
+struct Constants {
+    static let headerHeight = 36.0
+    static let cornerRadius = 20.0
+    static let margin = 20.0
+}
 
 class HomeDeliveryView: UIView {
+    
+    private lazy var headerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    private lazy var backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = Color.gray100
+        return view
+    }()
     
     override init(frame: CGRect = .zero) {
         super.init(frame: frame)
@@ -21,12 +40,26 @@ class HomeDeliveryView: UIView {
 
 extension HomeDeliveryView: ViewCodeProtocol {
     func setViewHierarchy() {
+        addSubview(headerView)
+        addSubview(backgroundView)
     }
     
     func setViewConstraints() {
+        headerView.snp.makeConstraints { make in
+            make.height.equalTo(Constants.headerHeight)
+            make.top.leading.trailing.equalToSuperview().inset(Constants.margin)
+        }
+        
+        backgroundView.snp.makeConstraints { make in
+            make.top.equalTo(headerView.snp.bottom).offset(Metrics.medium)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
     }
     
     func setViewConfigs() {
         backgroundColor = Color.redDark
+        backgroundView.layer.cornerRadius = Constants.cornerRadius
+        backgroundView.layer.masksToBounds = true
+        backgroundView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
     }
 }
