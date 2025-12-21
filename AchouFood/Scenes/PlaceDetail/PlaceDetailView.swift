255cd99fc1b9a2259325bab8618b9317c8732f46
arthur <arthur.rios007@gmail.com>
2025-12-21 13:37:10 -0300

    Tela de detalhes - Layout tela de detalhes - Etapa 1

--- AchouFood/Scenes/PlaceDetail/PlaceDetailView.swift
+++ AchouFood/Scenes/PlaceDetail/PlaceDetailView.swift
@@ -15,6 +15,7 @@ struct PlaceDetailConstants {
     static let backButtonImage = "backButton"
     static let padding = 20.0
     static let backImageSize = 36.0
+    static let contentTop = 153.0
 }
 
 class PlaceDetailView: UIView {
@@ -31,6 +32,37 @@ class PlaceDetailView: UIView {
         view.layer.borderColor = UIColor.white.cgColor
         view.layer.masksToBounds = true
         view.addGestureRecognizer(gesture)
+        return view
+    }()
+    
+    private lazy var placeImageView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFill
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 16
+        view.layer.borderWidth = 4
+        view.layer.borderColor = Color.gray100.cgColor
+        return view
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let view = UILabel()
+        view.font = Typography.titleLg
+        view.textColor = Color.gray600
+        return view
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let view = UILabel()
+        view.font = Typography.bodySm
+        view.textColor = Color.gray500
+        view.numberOfLines = 5
+        return view
+    }()
+    
+    private lazy var contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = Color.gray100
         return view
     }()
     
@@ -57,6 +89,10 @@ class PlaceDetailView: UIView {
 extension PlaceDetailView: ViewCodeProtocol {
     func setViewHierarchy() {
         addSubview(backButton)
+        addSubview(contentView)
+        addSubview(placeImageView)
+        addSubview(titleLabel)
+        addSubview(descriptionLabel)
     }
     
     func setViewConstraints() {
@@ -65,9 +101,33 @@ extension PlaceDetailView: ViewCodeProtocol {
             make.leading.equalToSuperview().offset(PlaceDetailConstants.padding)
             make.size.equalTo(PlaceDetailConstants.backImageSize)
         }
+        
+        contentView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(PlaceDetailConstants.contentTop)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        placeImageView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(93)
+            make.leading.equalToSuperview().inset(20)
+            make.size.equalTo(80)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(placeImageView.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(12)
+            make.leading.trailing.equalToSuperview().inset(20)
+        }
     }
     
     func setViewConfigs() {
         backgroundColor = Color.redDark
+        contentView.layer.cornerRadius = Metrics.medium
+        contentView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        contentView.layer.masksToBounds = true
     }
 }
