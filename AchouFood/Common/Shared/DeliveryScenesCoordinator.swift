9ab60830d5a8c2d187560f5cd3e49d4234120c95
arthur <arthur.rios007@gmail.com>
2025-12-26 21:08:23 -0300

    Tela de detalhes - Apresentando uma rota

--- AchouFood/Common/Shared/DeliveryScenesCoordinator.swift
+++ AchouFood/Common/Shared/DeliveryScenesCoordinator.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MapKit
 
 public class DeliveryScenesCoordinator {
     private var navigationController: UINavigationController?
@@ -29,6 +30,32 @@ extension DeliveryScenesCoordinator: HomeDeliveryCoordinator {
 
 // MARK: - PlaceDetail Coordinator
 extension DeliveryScenesCoordinator: PlaceDetailCoordinator {
+    public func traceRoute(_ origin: CLLocationCoordinate2D, _ destination: CLLocationCoordinate2D) {
+        let originPlacemark = MKPlacemark(coordinate: origin)
+        let destinationPlacemark = MKPlacemark(coordinate: destination)
+        
+        let originItem = MKMapItem(placemark: originPlacemark)
+        originItem.name = "Você"
+        let destinationItem = MKMapItem(placemark: destinationPlacemark)
+        destinationItem.name = "Destino"
+        
+        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving,
+                              MKLaunchOptionsShowsTrafficKey: true] as [String : Any]
+        MKMapItem.openMaps(with: [originItem, destinationItem], launchOptions: launchOptions)
+    }
+    
+    public func showAlert() {
+        let alert = UIAlertController(title: "alert.title".localized, message: "alert.message".localized, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "alert.cancel".localized, style: .cancel))
+        alert.addAction(UIAlertAction(title: "alert.settings".localized, style: .default, handler: { _ in
+            if let url = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(url)
+            }
+        }))
+        
+        navigationController?.present(alert, animated: true)
+    }
+    
     public func back() {
         navigationController?.popViewController(animated: true)
     }
